44a41c3555463febd32a5bc9b3954dd1b81468f1
federicob-0 <bragionfederico@gmail.com>
2026-03-28 18:23:28 +0100

    Update plates.py

--- pset2/plates.py
+++ pset2/plates.py
@@ -6,25 +6,21 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-# check one
-	if not s[0:2].isalpha():
-		return False
-# check two		
 	if not 2<= len(s) <=6:
 		return False
-# check three	
+	if not s.isalnum():
+		return False
+	if not s[0:2].isalpha():
+		return False
+		
 	found_number = False
 	for character in s:
 		if character.isdigit():
 			if character == "0" and not found_number:
 				return False
 			found_number = True
-		if found_number == True:
-			if character.isalpha():
-				return False	
-# check four					
-	if not s.isalnum():
-		return False
+		elif found_number:
+			return False
 	
 	return True
 main()
