8ca699eeec5dfbf326b57dfcdf0031486c1561e7
federicob-0 <bragionfederico@gmail.com>
2026-04-11 19:02:43 +0200

    Update outdated.py

--- pset3/outdated.py
+++ pset3/outdated.py
@@ -2,7 +2,7 @@ def main():
 	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 	month, day, year = get_date("Date: ", months)
 	format_date(month, day, year, months)
-    
+
 def get_date(prompt, months):
 	while True:
 		date = input(prompt).title()
@@ -15,12 +15,12 @@ def get_date(prompt, months):
 			if year > 0 and 1 <= month <= 12 and 1 <= day <= 31:
 				return month, day, year
 		try:
-			month, day, year = date.split(" ")
-			day, year = int(day.replace(",", "")), int(year)
+			month, day_comma, year = date.split(" ")
+			day, year = int(day_comma.replace(",", "")), int(year)
 		except ValueError:
 			pass
 		else:
-			if year > 0 and month in months and 1 <= day <= 31:
+			if day_comma.endswith(",") and year > 0 and month in months and 1 <= day <= 31:
 				return month, day, year
 
 def format_date(month, day, year, months):
